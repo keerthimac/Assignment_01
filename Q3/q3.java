e0e79cd2a05736ff14611ff5cddb63ee4e12b9f6
Darshana <keerthi.mac@gmail.com>
2023-07-13 08:30:41 +0530

    Working on

---
+++ Q3/q3.java
@@ -0,0 +1,9 @@
+class Example {
+
+    public static void main(String args[]) {
+        System.out.print("J");
+        System.out.print("A");
+        System.out.print("V");
+        System.out.print("A");
+    }
+}

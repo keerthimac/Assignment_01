93a8b7b7aa921bb778e536848a544c5a93c2c2d3
Darshana <keerthi.mac@gmail.com>
2023-07-14 00:38:39 +0530

    halfway

---
+++ Q4/q4.java
@@ -0,0 +1,9 @@
+class Example {
+
+    public static void main(String args[]) {
+        System.out.println('T');
+        System.out.println(1000);
+
+        System.out.println(1.23);
+    }
+}
---
+++ Q7/q7.java
@@ -0,0 +1,14 @@
+class Example {
+    public static void main(String args[]) {
+        System.out.println("1");
+        System.out.print("2");
+        System.out.println("3");
+        System.out.print("4");
+        System.out.print("5");
+        System.out.println("6");
+        System.out.print("7");
+        System.out.print("8");
+        System.out.print("9");
+        System.out.println("1O");
+    }
+}
